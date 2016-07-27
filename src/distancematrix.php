6258975f9a8372e81176a2a7268c50b8b4d624b9
Arnaud Weyts <arnaud.weyts@gmail.com>
2016-07-27 15:43:34 +0100

    deleted distancematrix file

--- src/distancematrix.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$key = 'AIzaSyAXTzdGTPfaSGz1wrCEmrqX1lJ3epVDA-0';
-	$from = $_GET['from'];
-	$to = $_GET['to'];
-	$mode = $_GET['mode'];
-	$url = 'https://maps.googleapis.com/maps/api/distancematrix/json…' . urlencode($from) . '&destinations=' . urlencode($to) . '&key='. urlencode($key) .'&travelmode=' . urlencode($mode) . '&departure_time=now';
-	$json = file_get_contents($url);
-	echo $json;
-?>
